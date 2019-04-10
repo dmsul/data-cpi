66cd6c96b88120400180da2b4a4312691bd6ed39
Daniel Sullivan <sullydm@gmail.com>
2019-04-10 12:07:17 -0400

    Add monthly and annual CPI all items urban

--- data_cpi/clean/raw.py
+++ data_cpi/clean/raw.py
@@ -1,12 +1,52 @@
 import os
 
+import pandas as pd
 import requests
 
 from util import src_path
 
 
-def download_all_items_series():
-    filepath = src_path('cu.data.1.AllItems.txt')
+def cpi_allitems_urban(monthly: bool=False) -> pd.DataFrame:
+    """ U.S. All items, 1982-84=100 """
+    df = load_raw_allitems_series()
+
+    # Select the right CPI series
+    series_id = 'CUUR0000SA0'
+    df['series_id'] = df['series_id'].str.strip()
+    df = df[df['series_id'] == series_id]
+    del df['series_id']
+
+    # Uniform below here (maybe period handling different)
+    df['footnote_codes'] = df['footnote_codes'].str.strip()
+    if df['footnote_codes'].str.len().max() == 0:
+        del df['footnote_codes']
+
+    if monthly:
+        df = df[df['period'] != 'M13']
+        df['month'] = (df['period']
+                       .str.replace('M', '')
+                       .astype(int))
+        idx_cols = ['year', 'month']
+        df = df.set_index()
+    else:
+        df = df[df['period'] == 'M13']
+        idx_cols = 'year'
+
+    df = df.set_index(idx_cols)
+
+    del df['period']
+
+    return df
+
+
+def load_raw_allitems_series() -> pd.DataFrame:
+    download_all_items_series()
+    df = pd.read_csv(allitems_src_path(), sep='\t')
+    return df
+
+
+def download_all_items_series() -> None:
+    filepath = allitems_src_path()
     if os.path.isfile(filepath):
         return
 
@@ -17,5 +57,9 @@ def download_all_items_series():
         f.write(page.text)
 
 
+def allitems_src_path() -> str:
+    return src_path('cu.data.1.AllItems.txt')
+
+
 if __name__ == "__main__":
-    pass
+    df = cpi_allitems_urban()
